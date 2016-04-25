31ed5f90e7a103d1bbd7d481bbe76725398e04aa
kiyoshiakuta <akuta.ky@gmail.com>
2016-04-25 10:39:32 -0400

    Finish 9.2

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,7 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -20,6 +20,10 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  def current_user?(user)
+    user == current_user
   end
 
   def logged_in?
@@ -36,6 +40,15 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 
 end
